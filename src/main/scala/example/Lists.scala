813db7cb507c1a59477058992555663a6b745891
Julio Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2017-02-06 18:25:23 +0100

    Refactor sum

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -23,7 +23,11 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = (xs foldLeft 0) (_+_)
+  def sum(xs: List[Int]): Int = {
+    if (xs.isEmpty) 0
+    else
+      xs.head + sum(xs.tail)
+  }
   
   /**
    * This method returns the largest element in a list of integers. If the
